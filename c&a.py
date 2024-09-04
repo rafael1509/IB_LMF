95c2153ef9e4e8394676969ec4c6c914bb8765c9
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2024-09-04 20:42:13 -0300

    Update c&a.py

--- c&a.py
+++ c&a.py
@@ -1,13 +1,17 @@
+'''
+Objetivo é conseguir as coordenadas de cada loja da C&A
+'''
+
 import pandas as pd
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 from time import sleep
 
 # Carregue sua planilha Excel
-planilha = pd.read_excel(r'C:\Users\rafae\OneDrive\Documentos\prog\coding\IB\data\c&a_data.xlsx')
+planilha = pd.read_excel(r'C:\Users\rafae\OneDrive\Documentos\prog\coding\IB_organized\data\c&a_data.xlsx')
 
 # Inicialize o geocodificador
-geolocator = Nominatim(user_agent="geoapiExercises")
+geolocator = Nominatim(user_agent="geocode_app")
 
 # Função para obter coordenadas (latitude e longitude) a partir do endereço, cidade e estado
 def get_coordinates(row):
@@ -22,8 +26,10 @@ def get_coordinates(row):
         return None, None
 
 
-# Adicione colunas de latitude e longitude à planilha
+# Adicionando colunas de latitude e longitude à planilha
 planilha['Latitude'], planilha['Longitude'] = zip(*planilha.apply(get_coordinates, axis=1))
 
-# Salve a planilha atualizada
-planilha.to_excel('sua_planilha_atualizada.xlsx', index=False)
+# Salvando
+# obs: o geopy nao consegue achar a localização exata de todas as lojas, então tive que
+# ajeitar alguns detalhes manualmente
+planilha.to_excel('output/c&a_coord.xlsx', index=False)
